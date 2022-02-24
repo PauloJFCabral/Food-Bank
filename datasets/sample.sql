d42b18294544b43c39edd2c6775e95b877d1d419
Paulo Cabral <paulojorgefreitascabral@gmail.com>
2022-02-24 15:59:36 +0000

    Add demo dataset

---
+++ datasets/sample.sql
@@ -0,0 +1,30 @@
+BEGIN TRANSACTION;
+CREATE TABLE "Account" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"ParentId" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Account" VALUES(1,'Sample Account for Entitlements','');
+INSERT INTO "Account" VALUES(2,'Paulo Cabral','');
+CREATE TABLE "Delivery_Item__c" (
+	id INTEGER NOT NULL, 
+	"Food_Expiration_Date__c" VARCHAR(255), 
+	"Food_Storage__c" VARCHAR(255), 
+	"Name" VARCHAR(255), 
+	"Delivery__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery_Item__c" VALUES(1,'','Frozen','Soup','1');
+INSERT INTO "Delivery_Item__c" VALUES(2,'','Non-refrigerated','Meat','2');
+CREATE TABLE "Delivery__c" (
+	id INTEGER NOT NULL, 
+	"Name" VARCHAR(255), 
+	"Scheduled_Date__c" VARCHAR(255), 
+	"Status__c" VARCHAR(255), 
+	"Supplier__c" VARCHAR(255), 
+	PRIMARY KEY (id)
+);
+INSERT INTO "Delivery__c" VALUES(1,'Delivery 2','','Scheduled','');
+INSERT INTO "Delivery__c" VALUES(2,'Delivery 1','','Request','');
+COMMIT;
